23b0a5df974b382e9ddaf7e84b366d88f8b3515e
Mariia Butolina <104693230+butolinka@users.noreply.github.com>
2023-01-10 00:03:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+
+    function toggleOpen() {
+      console.log('Hello');
+      this.classList.toggle('open');
+    }
+
+    function toggleActive(e) {
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    }
+
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
